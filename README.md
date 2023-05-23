8066e514247d6357f37e0e4e83df930ea445ca75
benjaminzakka <125729476+benjaminzakka@users.noreply.github.com>
2023-05-23 14:06:05 +0100

    commit 2

---
+++ README.md
@@ -0,0 +1,1 @@
+contains solutions to the Simple_shell project given by the ALX schools
---
+++ aux_lists2.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add_rvar_node - adds a variable at the end
+ * of a r_var list.
+ * @head: The head of the linked list.
+ * @lvar: The length of the variable.
+ * @val: The value of the variable.
+ * @lval: The length of the value.
+ * Return: address of the head.
+ */
+r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
+{
+	r_var *new, *temp;
+
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
+
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_rvar_list - frees a r_var list
+ * @head: The head of the linked list.
+ * Return: Null.
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
